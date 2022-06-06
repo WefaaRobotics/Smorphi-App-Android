8b7f7c3a9258ff1fb6475cf275e94dd2b96e7be0
Wefaa Robotics <105783236+WefaaRobotics@users.noreply.github.com>
2022-06-06 15:31:59 +0800

    Joy stick library

---
+++ Android_joystick/app/src/main/java/com/example/android_joystick/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.android_joystick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextViewAngleLeft;
+    private TextView mTextViewStrengthLeft;
+
+    private TextView mTextViewAngleRight;
+    private TextView mTextViewStrengthRight;
+    private TextView mTextViewCoordinateRight;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mTextViewAngleLeft = (TextView) findViewById(R.id.textView_angle_left);
+        mTextViewStrengthLeft = (TextView) findViewById(R.id.textView_strength_left);
+
+        JoystickView joystickLeft = (JoystickView) findViewById(R.id.joystickView_left);
+        joystickLeft.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                mTextViewAngleLeft.setText(angle + "°");
+                mTextViewStrengthLeft.setText(strength + "%");
+            }
+        });
+
+
+
+        /*mTextViewAngleRight = (TextView) findViewById(R.id.textView_angle_right);
+        mTextViewStrengthRight = (TextView) findViewById(R.id.textView_strength_right);
+        mTextViewCoordinateRight = findViewById(R.id.textView_coordinate_right);
+
+        final JoystickView joystickRight = (JoystickView) findViewById(R.id.joystickView_right);
+        joystickRight.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onMove(int angle, int strength) {
+                mTextViewAngleRight.setText(angle + "°");
+                mTextViewStrengthRight.setText(strength + "%");
+                mTextViewCoordinateRight.setText(
+                        String.format("x%03d:y%03d",
+                                joystickRight.getNormalizedX(),
+                                joystickRight.getNormalizedY())
+                );
+            }
+        });*/
+    }
+}
